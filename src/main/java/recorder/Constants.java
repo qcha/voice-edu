07e4ef6ba39978d0684448e6e8d50f82bebe8ba4
MikhailCh <mike_ch@mail.ru>
2019-02-24 13:14:16 +0300

    fix constants

--- src/main/java/recorder/Constants.java
+++ src/main/java/recorder/Constants.java
@@ -2,6 +2,7 @@ package recorder;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
+import java.io.File;
 
 public final class Constants {
     private Constants() {
@@ -18,11 +19,13 @@ public final class Constants {
     public final static long TIMER = 420; // seconds
     public final static int DURATION = 5; // seconds
 
+    private final static String FPS = File.pathSeparator;
+    private final static String CSV_DIR = Constants.class.getClassLoader().getResource("classification").getPath();
     public final static String SOURCE_DIR = "working";
-    public final static String PATH_TO_WAV_TRAIN = SOURCE_DIR + "\\wave";
-    public final static String PATH_TO_CSV_TRAIN = SOURCE_DIR + "\\classification\\wav_data_train.csv";
-    public final static String PATH_TO_WAV_TEST = SOURCE_DIR + "\\wave\\test";
-    public final static String PATH_TO_CSV_TEST = SOURCE_DIR + "\\classification\\wav_data_eval.csv";
+    public final static String PATH_TO_WAV_TRAIN = SOURCE_DIR;
+    public final static String PATH_TO_CSV_TRAIN = CSV_DIR + FPS + "wav_data_train.csv";
+    public final static String PATH_TO_WAV_TEST = SOURCE_DIR;
+    public final static String PATH_TO_CSV_TEST = CSV_DIR + FPS + "wav_data_eval.csv";
 
     public final static int NEURONS = 50; //кол-во входных нейронов, зависит от структуры записи
 }
